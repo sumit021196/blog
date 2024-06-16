25b96f1ce7ede3a78b7669eeca8c013c49735629
sumit021196 <154895578+sumit021196@users.noreply.github.com>
2024-06-16 19:17:30 +0530

    Update Footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,7 +3,7 @@ import React from 'react';
 const Footer = () => {
   return (
     <footer>
-      <p>© 2024 Travel Blog. All rights reserved.</p>
+      <p>© 2024 Travel & Food. All rights reserved.</p>
     </footer>
   );
 };
